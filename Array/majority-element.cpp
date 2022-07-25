9b1e918296226a8e417a9309c89286c11bf33450
lodhi-abhishek <lodhiabhishek.in@gmail.com>
2022-07-25 11:25:49 +0530

    removed file

---
+++ Array/majority-element.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Find elements that are more then floor n/k times
+
+Boyer–Moore majority vote algorithm used
+*/
+vector<int> MajorityElement(vector<int>&nums) {
+}
+
+int main(void) {
+    vector<int> nums = {3,2,3};
+
+    vector<int> result = MajorityElement(nums); 
+    return 0;
+}
