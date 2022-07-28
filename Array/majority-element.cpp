e1e4ae79fbbea7032489308a0dd1940e49f38c85
lodhi-abhishek <lodhiabhishek.in@gmail.com>
2022-07-28 21:38:51 +0530

    Jul28 Majority Element for both n/2 and n/3

--- Array/majority-element.cpp
+++ Array/majority-element.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*
+Do see this
+https://www.geeksforgeeks.org/boyer-moore-majority-voting-algorithm/
+*/
 
 /*
 Find elements that are more then floor n/k times
@@ -20,7 +24,7 @@ Step 2 – Check if the candidate has more than N/2 votes –
 */
 
 // ⌊n / 2⌋
-vector<int> MajorityElement(vector<int> &nums)
+vector<int> MajorityElementn2(vector<int> &nums)
 {
 
     int n = nums.size(), count = 0, element = -1, votes = 0;
@@ -51,10 +55,57 @@ vector<int> MajorityElement(vector<int> &nums)
 }
 
 // This function is for element ⌊ n/3 ⌋
+vector<int> MajorityElementn3(vector<int>& nums) {
+
+    int n = nums.size(), element1 = -1, element2 = -1;
+    int vote_element1 = 0, vote_element2 = 0; 
+    vector<int> result;
+
+    for(int i = 0; i < n; i++) {
+        if(nums[i] == element1) {
+            vote_element1++;
+        }
+
+        else if(nums[i] == element2) {
+            vote_element2++;
+        }
+
+        else if(vote_element1 == 0) {
+            element1 = nums[i];
+            vote_element1 = 1;
+        } 
+
+        else if(vote_element2 == 0) {
+            element2 = nums[i];
+            vote_element2 = 1;
+        }
+
+        else {
+            vote_element1--;
+            vote_element2--;
+        }
+    }
+
+    int count1 = 0, count2 = 0;
+    for(int i = 0; i < n; i++) {
+        if(nums[i] == element1)
+            ++count1;
+        else if(nums[i] == element2)
+            ++count2;
+    }
+
+    if(count1 > n/3) result.push_back(element1);
+    if(count2 > n/3) result.push_back(element2);
+
+    return result; 
+}
+
 int main(void) {
 
-    vector<int> nums = {3, 2, 3};
-    vector<int> result = MajorityElement(nums);
+    // vector<int> nums = {3, 2, 3};
+    // vector<int> result = MajorityElementn2(nums);
+    vector<int> nums = {-1,-1,-1};
+    vector<int> result = MajorityElementn3(nums);
 
     for (int i : result)
         cout << i << " ";
