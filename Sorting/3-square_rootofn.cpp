527bff71cafb59b33449f7254b291f5154cb8f6a
lodhi-abhishek <lodhiabhishek.in@gmail.com>
2022-10-05 16:53:04 +0530

    sqr root added

---
+++ Sorting/3-square_rootofn.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countsquare(int n) {
+        int count = 0;
+        
+        for(int i = 1; i< sqrt(n); i++) {
+            if( i * i < n )
+                ++count;
+        }
+        
+        return count;
+}
